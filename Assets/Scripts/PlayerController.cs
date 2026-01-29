e95217c8a3f0011ecf452064288b479bb7b67156
deboerm2 <90107341+deboerm2@users.noreply.github.com>
2026-01-29 16:49:21 -0500

    movement works

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,23 @@ public class PlayerController : MonoBehaviour
 {
 
     public float jumpStrength;
-    public float slideForce;
+    public float slideStrength;
 
     private Collider2D col;
     private Rigidbody2D rb;
     private float xInput;
     private float yInput;
     [SerializeField]
-    private bool isGroundedX;
-    [SerializeField]
+    [Tooltip("Grounded on a vertical plane")]
     private bool isGroundedY;
+    [SerializeField] [Tooltip("Grounded on a horizontal plane")]
+    private bool isGroundedX;
+    
+    //used to determine if a burst of movement on respective axis is available
+    public bool xBurst;
+    public bool yBurst;
+    private bool xBurstInput;
+    private bool yBurstInput;
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +37,38 @@ public class PlayerController : MonoBehaviour
         xInput = Input.GetAxis("Horizontal");
         yInput = Input.GetAxis("Vertical");
 
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            yBurstInput = true;
+            yInput = 1f;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            yBurstInput = true;
+            yInput = -1f;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            xBurstInput = true;
+            xInput = -1f;
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            xBurstInput = true;
+            xInput = 1f;
+        }
+            
+
         CheckGrounded();
+
+        if(isGroundedX || isGroundedY)
+        {
+            xBurst = true;
+            yBurst = true;
+        }
+
+        
+       
     }
 
     private void LateUpdate()
@@ -40,7 +78,37 @@ public class PlayerController : MonoBehaviour
 
     void Movement()
     {
-        rb.AddForce(new Vector2(xInput, yInput),ForceMode2D.Impulse);
+        //rb.AddForce(new Vector2(xInput, yInput),ForceMode2D.Impulse);
+        if(xInput != 0f)
+        {
+            if (isGroundedX)
+            {
+                rb.velocity = new Vector2(xInput, 0) * slideStrength;
+            }
+            else if (xBurst && xBurstInput)
+            {
+                rb.velocity = Vector2.zero;
+                rb.AddForce(new Vector2(xInput, 0) * jumpStrength,ForceMode2D.Impulse);
+                xBurst = false;
+            }
+            
+        }
+        if(yInput != 0f)
+        {
+            if (isGroundedY)
+            {
+                rb.velocity = new Vector2(0, yInput) * slideStrength;
+            }
+            else if (yBurst && yBurstInput)
+            {
+                rb.velocity = Vector2.zero;
+                rb.AddForce(new Vector2(0, yInput) * jumpStrength,ForceMode2D.Impulse);
+                yBurst = false;
+            }
+            
+        }
+        xBurstInput = false;
+        yBurstInput = false;
     }
 
     void CheckGrounded()
@@ -56,15 +124,15 @@ public class PlayerController : MonoBehaviour
 
         //Debug.DrawRay(col.bounds.center + (1.05f * col.bounds.extents.x * Vector3.left), Vector2.right * col.bounds.size.x * 1.05f, Color.white);
         if (Physics2D.Raycast(col.bounds.center + (1.05f * col.bounds.extents.x * Vector3.left), Vector2.right, col.bounds.size.x * 1.05f, layermask))
-            isGroundedX = true;
-        else
-            isGroundedX = false;
-
-        //Debug.DrawRay(col.bounds.center + (1.05f * col.bounds.extents.y * Vector3.down), Vector2.up * col.bounds.size.y * 1.05f, Color.white);
-        if (Physics2D.Raycast(col.bounds.center + (1.05f * col.bounds.extents.y * Vector3.down), Vector2.up, col.bounds.size.y * 1.05f, layermask))
             isGroundedY = true;
         else
             isGroundedY = false;
+
+        //Debug.DrawRay(col.bounds.center + (1.05f * col.bounds.extents.y * Vector3.down), Vector2.up * col.bounds.size.y * 1.05f, Color.white);
+        if (Physics2D.Raycast(col.bounds.center + (1.05f * col.bounds.extents.y * Vector3.down), Vector2.up, col.bounds.size.y * 1.05f, layermask))
+            isGroundedX = true;
+        else
+            isGroundedX = false;
 
     }
 }
